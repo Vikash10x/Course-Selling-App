3e6036004c8010fb212a86e1c2b873337181c45e
Vikash Kumawat <159568780+Vikash10x@users.noreply.github.com>
2026-04-09 19:22:10 +0530

    backend

--- backend/index.js
+++ backend/index.js
@@ -51,11 +51,9 @@ app.use("/api/v1/rating", ratingRouter);
 
 const PORT = process.env.PORT || 3000;
 
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 module.exports = app;
 
