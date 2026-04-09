db773232f9c468bde012d7c36aff0678e77e5c77
Vikash Kumawat <159568780+Vikash10x@users.noreply.github.com>
2026-04-09 19:44:20 +0530

    frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,4 +2,4 @@ const isProduction = window.location.hostname !== "localhost";
 
 export const API_BASE_URL = isProduction
     ? "/api/v1"
-    : "https://course-selling-app-ht6d.onrender.com/api/v1";
+    : "https://course-selling-app-ht6d.onrender.com/course";
